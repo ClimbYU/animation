de0ba4fa804b0d189deeb60acd5ac63bd280fbb2
ClimbYU <yumengkang@126.com>
2020-09-01 22:19:48 +0800

    update

--- util.js
+++ util.js
@@ -1,16 +1,53 @@
 
 
 function Utils() {
+  this.mouse = { x: 0, y: 0 };
+  this.touch = { x: null, y: null, isPressed: false };
 }
 // 计算鼠标在元素的相对位置
 Utils.prototype = {
+
+  // 触摸事件获取
+  captureTouch: function (element) {
+
+    element.addEventListener('touchstart', (event) => {
+      this.touch.isPressed = true
+
+    }, false)
+
+    element.addEventListener('touchend', (event) => {
+      // this.touch = { x: null, y: null, isPressed: false };
+
+    }, false)
+
+    element.addEventListener('touchmove', (event) => {
+      let x, y;
+      touchEvent = event.touches[0];
+      if (touchEvent.pageX || touchEvent.pageY) {
+        x = touchEvent.pageX
+        y = touchEvent.pageY
+      } else {
+        x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+        y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop
+      }
+
+      x -= (this.getElementLeft(element) + this.getBorderWidth(element))
+      y -= (this.getElementTop(element) + this.getBorderWidth(element))
+
+      this.touch.x = x;
+      this.touch.y = y;
+
+    }, false)
+
+    return this.touch
+
+  },
+
   // 鼠标位置相对于元素位置
   captureMouse: function (element) {
-    let mouse = { x: 0, y: 0 };
     console.log(this)
 
     element.addEventListener('mousemove', (event) => {
-      console
       let x, y;
       if (event.pageX || event.pageY) {
         x = event.pageX;
@@ -25,10 +62,10 @@ Utils.prototype = {
       x -= (this.getElementLeft(element) + this.getBorderWidth(element))
       y -= (this.getElementTop(element) + this.getBorderWidth(element))
 
-      mouse.x = x;
-      mouse.y = y;
+      this.mouse.x = x;
+      this.mouse.y = y;
     }, false)
-    return mouse
+    return this.mouse
   },
   getElementLeft: function (element) {
     let curLeft = element.offsetLeft;
